aad003e1e3f65947eff7ac75f177fa530c66c66f
Maksim Shamanov <m.shamanov@combo21.com>
2017-12-11 18:23:08 +0200

    add requests

--- src/Button.js
+++ src/Button.js
@@ -1,40 +1,53 @@
 import React from 'react';
 
 export class Button extends React.Component {
-  componentWillMount() {
-    fetch('https://5a2e6f220e07b70012083a6a.mockapi.io/img/images')
-      .then(
-        function(response) {
-          if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' +
-              response.status);
-            return;
-          }
-    
-          // Examine the text in the response
-          response.json().then(function(data) {
-            console.log(data);
-          });
-        }
-      )
-      .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-      });
-    alert('work');
-  }
-
-  
   constructor(props) {
     super(props);
     this.state = {
       
     };
   }
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch('https://5a2e6f220e07b70012083a6a.mockapi.io/img/images')
+    .then(response => {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+  
+        // Examine the text in the response
+        response.json()
+        .then(data => {
+          console.log(data);
+          this.setState({
+            data
+          });
+          console.log(this.state);
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
+  }
+  
   render() {
     
 
     return (
-      <button className="btn">1</button>
+      
+      this.state.data.map((key) => (
+        <button className="btn" key={key}>{key}</button>
+      ))
+
+      // <button className="btn">1</button>
+
     );
   }
 }
