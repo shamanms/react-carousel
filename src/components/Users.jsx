8de99685ae6b77f880bf57df6479665c69f63719
m.shamanov <m.shamanov21@combo21.com>
2018-01-19 18:48:26 +0200

    styles and begining for card editor

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -6,19 +6,49 @@ import PropTypes from 'prop-types';
 import '../css/userCards.css'
 
 export class Users extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menu: false,
+      selectedCard: null,
+    };
+
+    this.deleteCard = this.deleteCard.bind(this); 
+  }
+
+  deleteCard = (e, index) => {
+    e.preventDefault()
+    this.setState({
+      menu: true,
+      selectedCard: index,
+    })
+  }
+
   render () {
     return (
       <div className="wrapper">
         <div className="wrapper_cards">
           { this.props.userList.map((e, index) =>
-            <div key={index} className="card">
+            <div key={index} className="card" onContextMenu={(e) => this.deleteCard(e, index)}>
               <div className="name">{e.name}</div>
               <div className="email">{e.email}</div>
               <div className="message">{e.message}</div>
+              <div className={this.state.menu && index === this.state.selectedCard
+                              ? "card-menu shown" 
+                              : "card-menu hide"}>
+                <ul>
+                  <li>
+                    <button>edit</button>
+                  </li>
+                  <li>
+                    <button>remove</button>
+                  </li>
+                </ul>
+              </div>
             </div>
           )}
         </div>
-
         <Link to='/'>Back</Link>
       </div>
     )
